5b563523cef637dc5eab3b92d3043dbb09ad4ba4
Bruno Tannús <39497117+brunotannus@users.noreply.github.com>
2024-01-23 14:38:28 -0300

    first working version

--- src/schema/mutation/convertRecords.mutation.ts
+++ src/schema/mutation/convertRecords.mutation.ts
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'lodash';
 import {
   GraphQLNonNull,
   GraphQLID,
@@ -5,14 +6,17 @@ import {
   GraphQLBoolean,
   GraphQLString,
 } from 'graphql';
-import { getNextId } from '@utils/form';
-import { Form, Record, Resource, User } from '@models';
+import { getNextId, getOwnership } from '@utils/form';
+import { Form, Record, Resource, User, Version } from '@models';
 import extendAbilityForRecords from '@security/extendAbilityForRecords';
 import { RecordType, ResourceType } from '../types';
 import { logger } from '@services/logger.service';
 import { graphQLAuthCheck } from '@schema/shared';
 import { Types } from 'mongoose';
 import { Context } from '@server/apollo/context';
+import * as fs from 'fs';
+import * as path from 'path';
+import { hasInaccessibleFields } from './editRecord.mutation';
 
 /** Arguments for the convertRecord mutation */
 type ConvertRecordsArgs = {
@@ -20,8 +24,8 @@ type ConvertRecordsArgs = {
   initialType: string;
   newType: string;
   field: string;
-  selectedResource: string;
   popArray: string;
+  failedAction: string;
 };
 
 /** Conversion of type string to array
@@ -29,8 +33,12 @@ type ConvertRecordsArgs = {
  * @param value - string to convert
  * @returns - array of strings
  */
-function stringToArray(value: string): string[] {
-  return [value.toString()];
+function stringToArray(value: string): any {
+  try {
+    return [value.toString()];
+  } catch (err) {
+    return 'error';
+  }
 }
 
 /** Conversion of type array to string
@@ -40,13 +48,17 @@ function stringToArray(value: string): string[] {
  * @returns - string
  */
 function arrayToString(value: string[], popArray: string): string {
-  switch (popArray) {
-    case 'first':
-      return value[0];
-    case 'last':
-      return value[value.length - 1];
-    case 'all':
-      return value.toString();
+  try {
+    switch (popArray) {
+      case 'first':
+        return value[0];
+      case 'last':
+        return value[value.length - 1];
+      case 'all':
+        return value.toString();
+    }
+  } catch (err) {
+    return 'error';
   }
 }
 
@@ -55,27 +67,17 @@ function arrayToString(value: string[], popArray: string): string {
  * @param value - value to convert
  * @returns - boolean
  */
-function toBoolean(value: string): boolean {
-  if (Array.isArray(value)) {
-    return value.length > 0;
-  } else {
-    return !!value;
-  }
-}
-
-/** Conversion of text to resource.
- *
- * @param value - text to convert
- * @param newType - type of the new field
- * @returns - array of a single id
- */
-async function textToResource(value: string): Promise<string[]> {
-  const record: string = await Record.findOne({ name: value });
-  if (!record) {
-    //throw new GraphQLError(`Record with name ${value} does not exist.`);
-    console.log(`Record with name ${value} does not exist.`);
-  } else {
-    return [record];
+function toBoolean(value: any): any {
+  try {
+    if (Array.isArray(value)) {
+      return value.length > 0 ? true : false;
+    } else {
+      return !!value;
+    }
+  } catch (err) {
+    console.log(err);
+    console.log('a');
+    return 'error';
   }
 }
 
@@ -85,88 +87,100 @@ async function textToResource(value: string): Promise<string[]> {
  * @param args - conversionForm args
  * @returns - converted value
  */
-function convertFieldValue(value: any, args): any {
-  switch (args.initialType) {
-    case 'text':
-    case 'dropdown':
-    case 'radiogroup': {
-      switch (args.newType) {
-        case 'checkbox':
-        case 'tagbox':
-          return stringToArray(value);
-        case 'boolean':
-          return toBoolean(value);
-        case 'resource':
-          return textToResource(value);
-        case 'dropdown':
-        case 'radiogroup':
-        case 'text': {
-          //do nothing
-          break;
+async function convertFieldValue(value: any, args): Promise<any> {
+  try {
+    switch (args.initialType) {
+      case 'text':
+      case 'dropdown':
+      case 'radiogroup': {
+        switch (args.newType) {
+          case 'checkbox':
+          case 'tagbox':
+          case 'owner':
+          case 'users':
+            return stringToArray(value);
+          case 'boolean':
+            return toBoolean(value);
+          case 'dropdown':
+          case 'radiogroup':
+          case 'text': {
+            //do nothing
+            break;
+          }
         }
+        break;
       }
-      break;
-    }
-    case 'checkbox':
-    case 'tagbox': {
-      switch (args.newType) {
-        case 'text':
-        case 'dropdown':
-        case 'radiogroup': {
-          return arrayToString(value, args.popArray);
+      case 'checkbox':
+      case 'tagbox': {
+        switch (args.newType) {
+          case 'text':
+          case 'dropdown':
+          case 'radiogroup': {
+            return arrayToString(value, args.popArray);
+          }
+          case 'boolean': {
+            return toBoolean(value);
+          }
+          case 'checkbox':
+          case 'tagbox': {
+            //do nothing
+            break;
+          }
         }
-        case 'boolean': {
-          return toBoolean(value);
-        }
-        case 'checkbox':
-        case 'tagbox': {
-          //do nothing
-          break;
-        }
+        break;
       }
-      break;
-    }
-    case 'boolean': {
-      if (args.newType === 'text') {
-        return toBoolean(value);
-      }
-    }
-    case 'file':
-    case 'multipletext':
-    case 'matrix':
-    case 'expression':
-    case 'owner':
-    case 'users': {
-      if (args.newType === 'boolean') {
-        return toBoolean(value);
-      }
-    }
-    case 'resource': {
-      switch (args.newType) {
-        case 'resources': {
-          return stringToArray(value);
-        }
-        case 'boolean': {
+      case 'boolean': {
+        if (args.newType === 'text') {
           return toBoolean(value);
         }
       }
-      break;
-    }
-    case 'resources': {
-      switch (args.newType) {
-        case 'resource': {
-          return arrayToString(value, args.popArray);
-        }
-        case 'boolean': {
+      case 'file':
+      case 'multipletext':
+      case 'matrix':
+      case 'expression':
+      case 'owner':
+      case 'users': {
+        if (args.newType === 'boolean') {
           return toBoolean(value);
         }
       }
-      break;
+      case 'resource': {
+        switch (args.newType) {
+          case 'resources': {
+            return stringToArray(value);
+          }
+          case 'boolean': {
+            return toBoolean(value);
+          }
+        }
+        break;
+      }
+      case 'resources': {
+        switch (args.newType) {
+          case 'resource': {
+            return arrayToString(value, args.popArray);
+          }
+          case 'boolean': {
+            return toBoolean(value);
+          }
+        }
+        break;
+      }
+      default: {
+        console.log('b');
+        return 'error';
+      }
     }
-    default: {
-      return null;
-    }
+  } catch (err) {
+    console.log(err);
+    console.log('c');
+    return 'error';
   }
+}
+
+function writeToFile(filename: string, data: string): void {
+  const filePath = path.join('C:\\Users\\bruno\\Documents', filename);
+  fs.writeFileSync(filePath, data, 'utf-8');
 }
 
 /** Validate that the answer is in the question choices
@@ -191,27 +205,6 @@ function validateChoices(
   return true;
 }
 
-/** Validate that the answer is a record from the specified resource
- *
- * @param selectedResource - resource to validate
- * @param currentAnswer - answer to validate
- * @returns - true if the answer is a record from the specified resource, false otherwise
- */
-async function enforceResource(
-  selectedResource: string,
-  currentAnswer: string
-): Promise<boolean> {
-  if (selectedResource !== 'none') {
-    const record = await (
-      await Record.find({
-        resource: selectedResource,
-      })
-    ).map((record) => record.id);
-    return record.includes(currentAnswer) ? true : false;
-  }
-  return true;
-}
-
 /**
  * Convert all records from a given resource.
  *
@@ -223,14 +216,17 @@ export default {
     initialType: { type: new GraphQLNonNull(GraphQLString) },
     newType: { type: new GraphQLNonNull(GraphQLString) },
     field: { type: new GraphQLNonNull(GraphQLString) },
-    selectedResource: { type: GraphQLString },
     popArray: { type: GraphQLString },
     failedAction: { type: new GraphQLNonNull(GraphQLString) },
   },
   async resolve(parent, args: ConvertRecordsArgs, context: Context) {
-    graphQLAuthCheck(context);
+    graphQLAuthCheck(context); // TODO: deal with permissions
     try {
-      const oldRecords = await Record.find({ resource: args.id });
+      const oldRecords = await Record.find({ resource: args.id }).populate({
+        path: 'form',
+        model: 'Form',
+      });
+      const records: Record[] = [];
       const oldForm = await Promise.all(
         oldRecords.map((record) => Form.findById(record.form))
       );
@@ -240,6 +236,8 @@ export default {
         .flat()
         .find((field) => field.name === args.field);
 
+      const user = context.user;
+
       // Considering the user edited the form before converting the records, questionChoices refers to the new form
       const questionChoices = formFieldStructure['choices']?.map(
         (item) => item.value
@@ -248,31 +246,85 @@ export default {
         questionChoices.push('other');
         questionChoices.push('none');
       }
-      //console.log('BEFORE', JSON.stringify(oldRecords, null, 2));
 
-      oldRecords.forEach((record) => {
-        const data = record.data;
+      const beforeData = JSON.stringify(oldRecords, null, 2);
+      writeToFile('before.txt', 'BEFORE\n' + beforeData);
+
+      // Conversion loop
+      for (const record of oldRecords) {
+        let action = 'continue';
+        const data = cloneDeep(record.data);
         const currentAnswer = data[args.field];
+        const ability = await extendAbilityForRecords(user, record.form);
+        if (
+          ability.can('update', record) &&
+          !hasInaccessibleFields(record, data[args.field], ability) &&
+          currentAnswer
+        ) {
+          // Question choices validation
+          if (
+            (questionChoices &&
+              !validateChoices(questionChoices, currentAnswer)) ||
+            (await convertFieldValue(currentAnswer, args)) === 'error'
+          ) {
+            console.log('Error converting value');
+            action = args.failedAction;
+          }
+          if (action === 'continue') {
+            data[args.field] = await convertFieldValue(currentAnswer, args);
+            console.log('Value converted successfully');
+          }
+          if (action === 'delete') {
+            data[args.field] = null;
+            console.log('Value deleted successfully');
+          }
 
-        if (questionChoices && currentAnswer) {
-          if (!validateChoices(questionChoices, currentAnswer)) {
-            console.log('Invalid choices');
+          if (action !== 'ignore') {
+            const version = new Version({
+              createdAt: record.modifiedAt
+                ? record.modifiedAt
+                : record.createdAt,
+              data: record.data,
+              createdBy: user._id,
+            });
+            const update: any = {
+              data: { ...record.data, ...data },
+              _lastUpdatedBy: {
+                user: {
+                  _id: user._id,
+                  name: user.name,
+                  username: user.username,
+                },
+              },
+              $push: { versions: version._id },
+            };
+            const ownership = getOwnership(
+              record.form.fields,
+              data[args.field]
+            ); // Update with template during merge
+            Object.assign(
+              update,
+              ownership && { createdBy: { ...record.createdBy, ...ownership } }
+            );
+            const newRecord = await Record.findByIdAndUpdate(
+              record.id,
+              update,
+              {
+                new: true,
+              }
+            );
+            await version.save();
+            records.push(newRecord);
+            console.log('Conversion completed');
+            // to do: deal with cancel conversion
+            // change the mutation response in conversion.component.ts
+            // display success or failure message
           }
         }
-        if (args.selectedResource) {
-          if (!enforceResource(args.selectedResource, currentAnswer)) {
-            console.log('Invalid resource');
-          }
-        }
-
-        if (convertFieldValue(currentAnswer, args)) {
-          data[args.field] = convertFieldValue(currentAnswer, args);
-        } else {
-          console.log('Invalid conversion');
-        }
-      });
-
-      //console.log('AFTER', JSON.stringify(oldRecords, null, 2));
+      }
+      const afterData = JSON.stringify(records, null, 2);
+      writeToFile('after.txt', 'AFTER\n' + afterData);
+      return records;
     } catch (err) {
       logger.error(err.message, { stack: err.stack });
       if (err instanceof GraphQLError) {
