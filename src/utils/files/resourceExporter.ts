28944dac1ea258bb20b93063ef04413124375688
Antoine Hurard <antoine@reliefapplications.org>
2025-10-21 15:21:37 +0200

    fix: resource exporter could faild when using reference data fields

--- src/utils/files/resourceExporter.ts
+++ src/utils/files/resourceExporter.ts
@@ -193,7 +193,7 @@ export default class Exporter {
                     const subQueryField = queryField.subFields.find(
                       (z) => z.name === `${column.name}.${subColumn.name}`
                     );
-                    subColumn.title = subQueryField.title;
+                    subColumn.title = subQueryField?.title;
                   });
                 }
               }
