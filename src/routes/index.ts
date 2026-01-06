0bf246897ac5ca7e2a4750dec8688e5cdc37872c
Jose L. Garcia <42015332+Joselgc1@users.noreply.github.com>
2026-01-06 12:40:30 +0100

    refactor: Improve notification view (#118)
    MAB-564
    
    Co-authored-by: Antoine Hurard <antoine@reliefapplications.org>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,6 +11,7 @@ import roles from './roles';
 import gis from './gis';
 import style from './style';
 import events from './events';
+import notifications from './notifications';
 import config from 'config';
 
 /** Express router instance */
@@ -31,5 +32,6 @@ router.use('/roles', roles);
 router.use('/gis', gis);
 router.use('/style', style);
 router.use('/events', events);
+router.use('/notifications', notifications);
 
 export { router };
