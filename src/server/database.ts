bb9d560a4fca5b89f797113e226dc2a1d86726d1
Antoine Hurard <antoine@reliefapplications.org>
2026-06-16 09:36:21 +0200

    fix: add replica set to docker

--- src/server/database.ts
+++ src/server/database.ts
@@ -50,7 +50,7 @@ const mongoDBUrl = (): string => {
         'database.host'
       )}:${config.get('database.port')}/${config.get(
         'database.name'
-      )}?authSource=admin&retrywrites=false&maxIdleTimeMS=120000`;
+      )}?authSource=admin&replicaSet=rs0&directConnection=true&retrywrites=false&maxIdleTimeMS=120000`;
     }
   }
 };
